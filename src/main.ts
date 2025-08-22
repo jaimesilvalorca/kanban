3844fbed66f6235051c638609947adda9381dc8c
jaimesilvalorca <jaimesilvalorca@gmail.com>
2025-08-22 12:25:50 -0400

    comentarios

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors(),
+  //app.enableCors(),
+  //app.enableCors(),
+  //app.enableCors(),
+
 
   app.useGlobalPipes(
     new ValidationPipe({
